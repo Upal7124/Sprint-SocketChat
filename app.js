dbfca81dda4c2d35d0b628214ff4f6bd15ec7b6b
Upal <ghoshupal25@gmail.com>
2026-08-25 22:02:20 +0530

    Second Phase

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+const blogs = [];
 
 app.get("/", (req, res) => {
   res.send("Server running on port 5000");
 });
 
 app.get("/blogs", (req, res) => {
-  res.json({ message: "GET blogs" });
+  res.json(blogs);
 });
 
 app.post("/blogs", (req, res) => {
-  res.json({ message: "POST blog" });
+  const blog = {
+    id: blogs.length + 1,
+    ...req.body,
+  };
+  blogs.push(blog);
+  res.json(blog);
 });
 
 app.put("/blogs/:id", (req, res) => {
-  res.json({ message: "PUT blog" });
+  const id = Number(req.params.id);
+  const blog = blogs.find((blog) => blog.id === id);
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+  Object.assign(blog, req.body);
+  res.json(blog);
 });
 
 app.delete("/blogs/:id", (req, res) => {
-  res.json({ message: "DELETE blog" });
+  const id = Number(req.params.id);
+  const filteredBlogs = blogs.filter((blog) => blog.id !== id);
+  blogs.length = 0;
+  blogs.push(...filteredBlogs);
+  res.json({ message: "Blog deleted successfully" });
 });
 
 app.listen(5000, () => {
