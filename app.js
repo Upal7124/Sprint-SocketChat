0d31fee8cf678917903fcfaebd8fab4d849e2e9b
Upal <ghoshupal25@gmail.com>
2026-08-29 16:52:02 +0530

    Sprint-10 First Phase

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const Post = require("./models/post");
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
+
 app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
@@ -12,35 +27,65 @@ app.get("/", (req, res) => {
   res.send("Server running on port 5000");
 });
 
-app.get("/blogs", (req, res) => {
-  res.json(blogs);
-});
+app.get("/blogs", async (req, res) => {
+  try {
+    const posts = await Post.find();
 
-app.post("/blogs", (req, res) => {
-  const blog = {
-    id: blogs.length + 1,
-    ...req.body,
-  };
-  blogs.push(blog);
-  res.json(blog);
-});
-
-app.put("/blogs/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const blog = blogs.find((blog) => blog.id === id);
-  if (!blog) {
-    return res.status(404).json({ message: "Blog not found" });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  Object.assign(blog, req.body);
-  res.json(blog);
 });
 
-app.delete("/blogs/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const filteredBlogs = blogs.filter((blog) => blog.id !== id);
-  blogs.length = 0;
-  blogs.push(...filteredBlogs);
-  res.json({ message: "Blog deleted successfully" });
+app.post("/blogs", async (req, res) => {
+  try {
+    const post = await Post.create(req.body);
+
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.put("/blogs/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    res.json(post);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+app.delete("/blogs/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    res.json({
+      message: "Post deleted successfully",
+      post: post,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Invalid post ID",
+    });
+  }
 });
 
 app.post("/login", (req, res) => {
